4169df431d57d6f89d24814b8a92c8975904d3bc
Chetan <chetankharel7@gmail.com>
2023-06-10 13:05:34 +0545

    Update formula

--- Formula/envless.rb
+++ Formula/envless.rb
@@ -3,12 +3,18 @@ require "language/node"
 class Envless < Formula
   desc ""
   homepage "https://envless.dev"
-  url "https://s3.amazonaws.com/testingcli.envless.dev/versions/0.0.4/ad15691/envless-v0.0.4-ad15691-darwin-arm64.tar.xz"
-  sha256 "8ce5dfa9677b7cec08c1b6ca5b42c49c8f0df83afdfdea5de54ad02363e66afa"
+  url "https://s3.amazonaws.com/testingcli.envless.dev/channels/stable/envless-darwin-x64.tar.gz"
+  sha256 "[object Promise]"
   depends_on "node"
-   
 
-  def install
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://s3.amazonaws.com/testingcli.envless.dev/channels/stable/envless-darwin-arm64.tar.gz"
+      sha256 "[object Promise]"
+    end
+  end
+
+ def install
      inreplace "bin/envless", /^CLIENT_HOME=/, "export ENVLESS_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
      libexec.install Dir["*"]
      bin.install_symlink libexec/"bin/envless"
